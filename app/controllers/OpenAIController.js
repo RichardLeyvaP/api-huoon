2df281b6d05582d8f044d07c13e34858ff34ab8b
jimmbo89 <65252506+jimmbo89@users.noreply.github.com>
2024-12-20 14:24:49 -0500

    Update OpenAIController.js

--- app/controllers/OpenAIController.js
+++ app/controllers/OpenAIController.js
@@ -1,14 +1,26 @@
 const logger = require('../../config/logger'); // Importa el logger
 const openai  = require('../../config/openaiClient'); // Importa el logger
-
+const userConversations = {};
 module.exports = {
     // Función para obtener la respuesta de OpenAI
 async getAIResponse(req, res) {
+  logger.info(`${req.user.name} - Entrando a Chat con AI`);
+  logger.info('datos recibidos al preguntar');
+  logger.info(JSON.stringify(req.body));
     const { question, issue } = req.body;
-  
+    const userId = req.user.id;
     if (!question) {
       return res.status(400).json({ error: 'Pregunta es requerida' });
     }
+
+    // Verificar si el usuario tiene un historial de conversación
+    if (!userConversations[userId]) {
+      userConversations[userId] = [];
+    }
+
+    // Agregar el mensaje del usuario al historial
+    const userMessage = { role: 'user', content: question };
+    userConversations[userId].push(userMessage);
   
     try {
       // Realizar la solicitud a OpenAI
@@ -16,11 +28,16 @@ async getAIResponse(req, res) {
         model: "gpt-4o-mini",// Puedes usar "gpt-3.5-turbo" si prefieres
       messages: [
         { role: 'system', content: issue },
-        { role: 'user', content: question }
+        ...userConversations[userId]  // Pasar el historial de la conversación
       ],
       max_tokens: 300
     });
   
+    // Obtener la respuesta de OpenAI
+    const aiMessage = { role: 'assistant', content: response.choices[0].message.content };
+    
+    // Agregar la respuesta de la IA al historial
+    userConversations[userId].push(aiMessage);
       // Enviar la respuesta de OpenAI
       res.json({ question, answer: response.choices[0].message.content });
     } catch (error) {
